6e74173a4ef50fd28cb0a1cbd08ab3c1180da644
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-04-05 11:40:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+// const driverArray = []
+// function findMatching(collection, namefilter){
+//     for(const driver of collection){
+        
+//         if(driver === namefilter){
+//             // return driver
+//             // console.log(driver)
+//             driverArray.push(driver)
+            
+//         }
+//         //  console.log(driverArray)
+//     }
+//     // return driverArray
+//     console.log(driverArray)
+// }
+//  findMatching(drivers, 'Bobby')
+//  findMatching(drivers, 'Sammy')
+//  console.log(driverArray)
+
+
+function findMatching(collection, namefilter){
+    return collection.filter(
+        (result) => result.toLowerCase() === namefilter.toLowerCase()
+    );
+}
+
+
+function fuzzyMatch(collection, nameString){
+    return collection.filter((result) => 
+    result.toLowerCase().indexOf(nameString.toLowerCase())===0)
+}
+
+function matchName(collection, nameDetails){
+    return collection.filter((result) => result.name===nameDetails)
+}
